2f5ce3f2935bcc2bd729543ac69876a30cdc7c25
willpowr <hwilliams@willpoweru.com>
2018-06-21 09:04:26 +0100

    Added settings fragment and activity for server address

---
+++ app/src/main/java/com/willpoweru/audiowingsdemo/SettingsFragment.java
@@ -0,0 +1,49 @@
+package com.willpoweru.audiowingsdemo;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
+
+        addPreferencesFromResource(R.xml.preferences);
+        setHasOptionsMenu(true);
+
+        // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+        // to their values. When their values change, their summaries are
+        // updated to reflect the new value, per the Android Design
+        // guidelines.
+        SettingsActivity.bindPreferenceSummaryToValue(findPreference(getResources().getString(R.string.pref_key_audiowings_server_address)));
+
+
+    }
+
+
+
+
+}
